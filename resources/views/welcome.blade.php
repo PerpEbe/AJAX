2cd490217daa919e1944f42eec9764b1cfa8b505
PerpEbe <ebenezerperplex010@gmail.com>
2024-03-28 17:37:26 +0300

    Ajax Laravel crud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    {{-- Ajac cdn link --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </head>
@@ -43,7 +44,6 @@
                         jQuery('#message').html(result.message);
                         jQuery('#addpost')[0].reset();
                     }
-
                 });
             });
         });
